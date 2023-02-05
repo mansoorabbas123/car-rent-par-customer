c64562ceb0cd2f2dfbab997e539d6400dfd8eea4
hamza <mhamza889@gmail.com>
2023-02-05 16:15:04 +0500

    add homepage design

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <section className="bg-current">index</section>;
+}
+
+export default Footer;
---
+++ src/pages/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className=" bg-clip-content p-4 border-4 border-dashed border-orange-500 bg-gray-300 mt-10">
+      <div className="grid grid-cols-2 gap-4 mx-20 my-20 ">
+        <div>
+          <h2 className="text-5xl font-bold text-center text-orange-500 pt-6 ">
+            ABOUT US
+          </h2>
+          <p className="px-10 py-6 text-lg">
+            HMS limo service in pakistan is the premier private car service in
+            pakistan. Our expert drivers offer the highest quality of
+            professionalism, comfort, and safety. When you reserve a ride with
+            us, have the peace of mind that your driver will be prompt and will
+            meet all of your transportation needs. We specialize in corporate,
+            special event, and airport car service. HMS Limo Service has a large
+            fleet of luxury vehicles to select from.
+          </p>
+          <p className="px-10 py-6 text-lg">
+            HMS limo service in pakistan is the premier private car service in
+            pakistan. Our expert drivers offer the highest quality of
+            professionalism, comfort, and safety. When you reserve a ride with
+            us, have the peace of mind that your driver will be prompt and will
+            meet all of your transportation needs. We specialize in corporate,
+            special event, and airport car service. HMS Limo Service has a large
+            fleet of luxury vehicles to select from.
+          </p>
+        </div>
+        <div
+          className=" bg-cover bg-no-repeat bg-center "
+          style={{
+            backgroundImage: `url("/images/hamza.webp")`,
+          }}
+        ></div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
